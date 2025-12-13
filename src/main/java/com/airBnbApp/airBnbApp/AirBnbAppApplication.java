fcb86e6dfcf4487de99a1130f075b159a879bcb5
Akshit <akshit07032001@gmail.com>
2025-12-13 13:05:31 +0530

    init & db connection

---
+++ src/main/java/com/airBnbApp/airBnbApp/AirBnbAppApplication.java
@@ -0,0 +1,13 @@
+package com.airBnbApp.airBnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbAppApplication.class, args);
+	}
+
+}
